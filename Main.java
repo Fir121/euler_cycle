36524b3a7aae8a3199099b2806929d8d8284f96e
Fir121 <mohamedfiras2003@gmail.com>
2022-11-26 19:26:37 +0400

    User friendly

--- Main.java
+++ Main.java
@@ -6,24 +6,27 @@ public class Main {
         // defaults to 0
         int startVertex = 0;
         try{
+            int graphSize = args.length; 
             if (args[args.length-2].trim().equals("start")){
                 startVertex = Integer.parseInt(args[args.length-1]);
+                graphSize -= 2;
             }
-            adjMat = new int[args.length][args.length];
-            for (int i=0;i<args.length;i++){
+
+            adjMat = new int[graphSize][graphSize];
+            for (int i=0;i<graphSize;i++){
                 String[] temp = args[i].split(" ");
-                for (int j=0; j<args.length; j++){
+                for (int j=0; j<graphSize; j++){
                     adjMat[i][j] = Integer.valueOf(temp[j]);
                 }
             }
         }
         catch(Exception e){
-            System.out.println("Incorrect usage of Command Line Arguments - Specify Adjacency Matrix [format '01' '10']");
+            System.out.println("Incorrect usage of Command Line Arguments - Specify Adjacency Matrix [format '01' '10']\nOptional: Specify starting vertice value format:[start 1]");
             return;
         }
 
         if (args.length == 0){
-            System.out.println("No arguments specified, defaulting to sample graph -> '0 1 1 1 1' '1 0 1 0 0' '1 1 0 0 0' '1 0 0 0 1' '1 0 0 1 0'");
+            System.out.println("No arguments specified, defaulting to sample graph -> '0 1 1 1 1' '1 0 1 0 0' '1 1 0 0 0' '1 0 0 0 1' '1 0 0 1 0' start 1");
             adjMat = new int[][]{{0,1,1,1,1},{1,0,1,0,0},{1,1,0,0,0},{1,0,0,0,1},{1,0,0,1,0}};
         }
 
